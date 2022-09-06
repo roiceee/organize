d48523463427e9fe028b90fcd9b41496281d727f
jroiceee <jroicealdeza@gmail.com>
2022-09-07 02:08:55 +0800

    add next js initial files

--- src/script/index.js
+++
@@ -1,9 +0,0 @@
-import startAppLogic from "./initialLoad.js";
-
-// function addIcon() {
-//     const head = document.head;
-//     head.appendChild(document.createRange().createContextualFragment(`<link rel="shortcut icon" href="${logo}" type="image/x-icon">`));
-// }
-
-startAppLogic();
-// addIcon();
