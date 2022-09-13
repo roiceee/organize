03ba6520e232cae85bd3436080d2221629a3bb4d
jroiceee <jroicealdeza@gmail.com>
2022-09-13 20:16:47 +0800

    move project form as different component

---
+++ src/components/projects-components/edit-project-modal.tsx
@@ -0,0 +1,14 @@
+import ModalWrapper from "../util-components/modal-wrapper";
+
+function EditProjectModal() {
+    return ( 
+        <ModalWrapper
+        modalTitle="Edit Project"
+        bodyChildren={
+            
+        }
+        />
+     );
+}
+
+export default EditProjectModal;
