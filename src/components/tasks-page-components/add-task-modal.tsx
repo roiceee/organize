8f1ddf1e93a4c1d97c3151504b7e73196ba7de7a
jroiceee <jroicealdeza@gmail.com>
2022-09-17 14:03:54 +0800

    separate task form component from modal

--- src/components/tasks-page-components/add-task-modal.tsx
+++ src/components/tasks-page-components/add-task-modal.tsx
@@ -1,18 +1,14 @@
 import React, { useCallback, useState, useRef, useContext } from "react";
 import ModalWrapper from "../util-components/modal-wrapper";
-import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import TaskConstraintsEnum from "../../enums/task-constraints";
-import FormLengthCounter from "../util-components/form-length-counter";
 import TaskInterface from "../../interfaces/task-interface";
 import createTaskObject from "../../defaults/default-task";
 import {
   validateExistingTask,
   validateRequiredInput,
-  removeErrorFields,
 } from "../../utils/validation";
-import ProjectInterface from "../../interfaces/project-interface";
 import ProjectContext from "../../contexts/project-context";
+import TaskForm from "./task-form";
 
 interface AddTaskModalProps {
   showState: boolean;
@@ -30,22 +26,18 @@ function AddTaskModal({
   );
 
   const { currentProjectState } = useContext(ProjectContext);
-  const taskTitleForm = useRef(null);
+  const taskTitleFormRef = useRef(null);
 
   const areFormsValid = useCallback((): boolean => {
     return (
-      validateRequiredInput(taskTitleForm, "task-title-error") &&
+      validateRequiredInput(taskTitleFormRef, "task-title-error") &&
       validateExistingTask(
-        taskTitleForm,
+        taskTitleFormRef,
         "task-title-error",
         currentProjectState
       )
     );
-  }, [taskTitleForm, currentProjectState]);
-
-  const titleOnFocusHandler = useCallback(() => {
-    removeErrorFields(taskTitleForm, "task-title-error");
-  }, [taskTitleForm]);
+  }, [taskTitleFormRef, currentProjectState]);
 
   const taskTitleFormHandler = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -105,81 +97,14 @@ function AddTaskModal({
       showState={showState}
       onHide={onHide}
       bodyChildren={
-        <Form onSubmit={(e) => e.preventDefault()}>
-          <div className="d-flex justify-content-between mb-1">
-            <div>Title (required)</div>
-            <FormLengthCounter
-              currentValue={currentTaskState.title.length}
-              maxValue={TaskConstraintsEnum.TitleLength}
-            />
-          </div>
-          <Form.Control
-            autoFocus
-            type="text"
-            placeholder="Task title"
-            maxLength={TaskConstraintsEnum.TitleLength}
-            value={currentTaskState.title}
-            onChange={taskTitleFormHandler}
-            onFocus={titleOnFocusHandler}
-            ref={taskTitleForm}
-          />
-          <div className="error my-1" id="task-title-error"></div>
-          <div className="d-flex justify-content-between mb-1">
-            <div>Description</div>
-            <FormLengthCounter
-              currentValue={currentTaskState.description.length}
-              maxValue={TaskConstraintsEnum.DescriptionLength}
-            />
-          </div>
-          <Form.Control
-            as="textarea"
-            placeholder="Task Description"
-            className="mb-2"
-            style={{ height: "90px" }}
-            maxLength={TaskConstraintsEnum.DescriptionLength}
-            value={currentTaskState.description}
-            onChange={taskDescriptionFormHandler}
-          />
-          <div className="d-flex gap-2 align-items-center">
-            <div>Deadline:</div>
-            <Form.Control
-              type="date"
-              style={{ width: "fit-content" }}
-              className="mb-2"
-              value={currentTaskState.deadline}
-              onChange={deadLineFormHandler}
-            />
-          </div>
-          <div className="d-flex gap-2">
-            <div>Priority: </div>
-            <div key={`inline-radio`} className="mb-2 gap-1">
-              <Form.Check
-                inline
-                label="Low"
-                name="priority"
-                type="radio"
-                value="low"
-                onChange={priorityFormHandler}
-              />
-              <Form.Check
-                inline
-                label="Medium"
-                name="priority"
-                type="radio"
-                value="medium"
-                onChange={priorityFormHandler}
-              />
-              <Form.Check
-                inline
-                label="High"
-                name="priority"
-                type="radio"
-                value="high"
-                onChange={priorityFormHandler}
-              />
-            </div>
-          </div>
-        </Form>
+        <TaskForm
+          formTaskState={currentTaskState}
+          taskTitleFormRef={taskTitleFormRef}
+          titleFormHandler={taskTitleFormHandler}
+          descriptionFormHandler={taskDescriptionFormHandler}
+          deadlineFormHandler={deadLineFormHandler}
+          priorityFormHandler={priorityFormHandler}
+        />
       }
       footerChildren={
         <Button variant="secondary" onClick={addTaskButtonHandler}>
---
+++ src/components/tasks-page-components/task-form.tsx
@@ -0,0 +1,108 @@
+import Form from "react-bootstrap/Form";
+import TaskConstraintsEnum from "../../enums/task-constraints";
+import TaskInterface from "../../interfaces/task-interface";
+import FormLengthCounter from "../util-components/form-length-counter";
+import { removeErrorFields } from "../../utils/validation";
+import { useCallback } from "react";
+
+interface TaskFormProps {
+  formTaskState: TaskInterface;
+  taskTitleFormRef: React.RefObject<HTMLInputElement>;
+  titleFormHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  descriptionFormHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  deadlineFormHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  priorityFormHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function TaskForm({
+  formTaskState,
+  taskTitleFormRef,
+  titleFormHandler,
+  descriptionFormHandler,
+  deadlineFormHandler,
+  priorityFormHandler,
+}: TaskFormProps) {
+  const titleOnFocusHandler = useCallback(() => {
+    removeErrorFields(taskTitleFormRef, "task-title-error");
+  }, [taskTitleFormRef]);
+
+  return (
+    <Form onSubmit={(e) => e.preventDefault()}>
+      <div className="d-flex justify-content-between mb-1">
+        <div>Title (required)</div>
+        <FormLengthCounter
+          currentValue={formTaskState.title.length}
+          maxValue={TaskConstraintsEnum.TitleLength}
+        />
+      </div>
+      <Form.Control
+        autoFocus
+        type="text"
+        placeholder="Task title"
+        maxLength={TaskConstraintsEnum.TitleLength}
+        value={formTaskState.title}
+        onChange={titleFormHandler}
+        onFocus={titleOnFocusHandler}
+        ref={taskTitleFormRef}
+      />
+      <div className="error my-1" id="task-title-error"></div>
+      <div className="d-flex justify-content-between mb-1">
+        <div>Description</div>
+        <FormLengthCounter
+          currentValue={formTaskState.description.length}
+          maxValue={TaskConstraintsEnum.DescriptionLength}
+        />
+      </div>
+      <Form.Control
+        as="textarea"
+        placeholder="Task Description"
+        className="mb-2"
+        style={{ height: "90px" }}
+        maxLength={TaskConstraintsEnum.DescriptionLength}
+        value={formTaskState.description}
+        onChange={descriptionFormHandler}
+      />
+      <div className="d-flex gap-2 align-items-center">
+        <div>Deadline:</div>
+        <Form.Control
+          type="date"
+          style={{ width: "fit-content" }}
+          className="mb-2"
+          value={formTaskState.deadline}
+          onChange={deadlineFormHandler}
+        />
+      </div>
+      <div className="d-flex gap-2">
+        <div>Priority: </div>
+        <div key={`inline-radio`} className="mb-2 gap-1">
+          <Form.Check
+            inline
+            label="Low"
+            name="priority"
+            type="radio"
+            value="low"
+            onChange={priorityFormHandler}
+          />
+          <Form.Check
+            inline
+            label="Medium"
+            name="priority"
+            type="radio"
+            value="medium"
+            onChange={priorityFormHandler}
+          />
+          <Form.Check
+            inline
+            label="High"
+            name="priority"
+            type="radio"
+            value="high"
+            onChange={priorityFormHandler}
+          />
+        </div>
+      </div>
+    </Form>
+  );
+}
+
+export default TaskForm;
