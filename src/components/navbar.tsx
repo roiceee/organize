1aa6a56921c91dcd46d3ff36c84cf8c993954205
jroiceee <jroicealdeza@gmail.com>
2022-10-10 21:39:30 +0800

    add navbar highlighting

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useContext, useRef, useState } from "react";
+import { useRouter } from "next/router";
+import { useContext, useEffect, useRef, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import UserTypeContext from "../contexts/user-context";
@@ -9,6 +10,7 @@ import homeIcon from "../images/home.svg";
 import aboutIcon from "../images/info.svg";
 import { isLoggedInUser, isNotUser } from "../utils/user-checks";
 import UserDiv from "./user-div";
+import utilStyles from "../styles/modules/util-styles.module.scss";
 
 enum NavigationBarConstants {
   titleCollapseBreakPoint = 310,
@@ -18,10 +20,11 @@ function NavigationBar() {
   const { userTypeState } = useContext(UserTypeContext);
   const navbarCollapseToggle = useRef<HTMLButtonElement>(null);
 
+  const router = useRouter();
   const [viewportSizeState, setViewportSizeState] = useState(
     Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
   );
-
+ 
   onresize = () => {
     setViewportSizeState(
       Math.max(
@@ -30,6 +33,21 @@ function NavigationBar() {
       )
     );
   };
+
+  //loop through all navbar items and highlight current page
+  useEffect(() => {
+    
+    const navItems = document.querySelectorAll(".activable");
+    navItems.forEach((navItem) => {
+      const linkID = navItem.getAttribute("data-link");
+      if (navItem.classList.contains(utilStyles.borderBottomWhite)) {
+        navItem.classList.remove(utilStyles.borderBottomWhite);
+      }
+      if (linkID === router.asPath) {
+        navItem.classList.add(utilStyles.borderBottomWhite);
+      }
+    });
+  }, [ router]);
   return (
     <Navbar
       key="md"
@@ -58,14 +76,14 @@ function NavigationBar() {
         </div>
         {!isNotUser(userTypeState) && (
           <div className="d-flex gap-3 align-items-center">
-            <Navbar.Text className="mt-1">
+            <Navbar.Text className="mt-1 activable" data-link="/">
               <Link href="/">
                 <a className="text-light text-decoration-none">
                   <Image src={homeIcon} alt="Home" />
                 </a>
               </Link>
             </Navbar.Text>
-            <Navbar.Text className="mt-1">
+            <Navbar.Text className="mt-1 activable" data-link="/about">
               <Link href="/about">
                 <a className="text-light text-decoration-none">
                   <Image src={aboutIcon} alt="About" />
@@ -79,12 +97,12 @@ function NavigationBar() {
         )}
         {isNotUser(userTypeState) && (
           <div className="d-flex gap-3 align-items-center">
-            <Navbar.Text>
+            <Navbar.Text className="activable" data-link="/login">
               <Link href="/login">
                 <a className="text-light text-decoration-none">Log in</a>
               </Link>
             </Navbar.Text>
-            <Navbar.Text>
+            <Navbar.Text className="activable" data-link="/about">
               <Link href="/about">
                 <a className="text-light text-decoration-none">About</a>
               </Link>
