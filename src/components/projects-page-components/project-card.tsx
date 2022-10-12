169ed82ccab15ecded51f7ff74d2bdea1d30d468
jroiceee <jroicealdeza@gmail.com>
2022-10-12 22:02:15 +0800

    remove project card color to remove confusion with task cards

--- src/components/projects-page-components/project-card.tsx
+++ src/components/projects-page-components/project-card.tsx
@@ -16,7 +16,7 @@ function ProjectCard({ project }: ProjectCardProps) {
   return (
     <Link href={`/projects/${project.id}`}>
       <div
-        className={`${styles.cardWrapper} ${styles.rounded} ${utilStyles.hoverable} bg-white px-0 border border-1`}
+        className={`${styles.cardWrapper} ${styles.rounded} ${utilStyles.hoverable} bg-white px-0 border border-2`}
         tabIndex={0}
         style={{
           maxWidth: "400px",
@@ -24,22 +24,19 @@ function ProjectCard({ project }: ProjectCardProps) {
           wordBreak: "break-word",
         }}
       >
-        <div className="d-flex gap-1">
-          <div className={styles.verticalLine}></div>
-          <div className="p-1">
-            <h4 className="my-0 mb-2">{project.title}</h4>
-            <div style={{ fontSize: "0.9rem" }}>
-              <CardDetailRow
-                label="Tasks"
-                valueSpan={<span>{project.tasks.length}</span>}
-                svg={taskSVG}
-              />
-              <CardDetailRow
-                label="Last Modified"
-                valueSpan={<span>{formatDate(project.lastModified)}</span>}
-                svg={dateSVG}
-              />
-            </div>
+        <div className="p-1">
+          <h3 className="my-0 mb-2">{project.title}</h3>
+          <div style={{ fontSize: "0.9rem" }}>
+            <CardDetailRow
+              label="Tasks"
+              valueSpan={<span>{project.tasks.length}</span>}
+              svg={taskSVG}
+            />
+            <CardDetailRow
+              label="Last Modified"
+              valueSpan={<span>{formatDate(project.lastModified)}</span>}
+              svg={dateSVG}
+            />
           </div>
         </div>
       </div>
