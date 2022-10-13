2b45b9949c536ba66f8968881d41d4f028de2e28
jroiceee <jroicealdeza@gmail.com>
2022-10-13 20:54:31 +0800

    add theme color content in head tag

--- src/components/head-wrapper.tsx
+++ src/components/head-wrapper.tsx
@@ -10,7 +10,7 @@ function HeadWrapper({ title }: HeadWrapperProps) {
       <Head>
         <title>{!title ? "Organize" : `${title} | Organize`}</title>
         <meta name="description" content="Lightweight Task Manager" />
-        <meta name="theme-color"/>
+        <meta name="theme-color" content="#2236C2"/>
         <link rel="apple-touch-icon" href="/icon192.png"/>
         <link rel="icon" href="/icon.png" />
         <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials"/>
