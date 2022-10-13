cb2baa530e6ca25ab6d8344c86480bf316a32db0
jroiceee <jroicealdeza@gmail.com>
2022-10-13 20:01:53 +0800

    add manifest

--- src/components/head-wrapper.tsx
+++ src/components/head-wrapper.tsx
@@ -1,17 +1,17 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
 
 interface HeadWrapperProps {
   title?: string;
 }
 function HeadWrapper({ title }: HeadWrapperProps) {
-  const router = useRouter();
+  
   return (
     <>
       <Head>
         <title>{!title ? "Organize" : `${title} | Organize`}</title>
         <meta name="description" content="Lightweight Task Manager" />
         <link rel="icon" href="/organize-icon.png" />
+        <link rel="manifest" href="/webmanifest.json" crossOrigin="use-credentials"/>
       </Head>
     </>
   );
