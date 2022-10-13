e5f826087911932c3d48ec96fd6a7c4703c3896e
jroiceee <jroicealdeza@gmail.com>
2022-10-13 18:24:21 +0800

    add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
 const withCss = require("@zeit/next-css");
 const withPurgeCss = require("next-purgecss");
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({})
 
 module.exports = withCss(withPurgeCss());
 module.exports = nextConfig
