c22591a2d6be3a007d9dff992bc5694ccd63611e
jroiceee <jroicealdeza@gmail.com>
2022-10-14 16:32:43 +0800

    add next-pwa plugin

--- next.config.js
+++ next.config.js
@@ -2,33 +2,32 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
 
+    return config;
+  },
+};
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
 
 const withCss = require("@zeit/next-css");
 const withPurgeCss = require("next-purgecss");
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-module.exports = withBundleAnalyzer({})
+module.exports = withBundleAnalyzer({});
 
 module.exports = withCss(withPurgeCss());
-module.exports = nextConfig
 
-module.exports = {
-  images: {
-    domains: ['lh3.googleusercontent.com'],
-  },
-  webpack: (config) => {
-    config.module.rules.push(
-      {
-        test: /\.md$/,
-        use: 'raw-loader'
-      }
-    )
-
-    return config
-  },
-}
+module.exports = withPWA(nextConfig);
