216d689e3edf3c2ecf997a97334a5ec3d810bb2a
jroiceee <jroicealdeza@gmail.com>
2022-10-15 21:57:24 +0800

    update PWA

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,9 @@ import { NetworkOnly, NetworkFirst, CacheFirst, StaleWhileRevalidate } from 'wor
 import { registerRoute, setDefaultHandler, setCatchHandler } from 'workbox-routing';
 import { matchPrecache, precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
 
+//change value to trigger update of PWA
+const VERSION = 1
+
 self.skipWaiting();
 clientsClaim();
 
